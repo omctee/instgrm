86b206268082e42261c3dfe040d896b915e6609f
None None <none@none.com>
2018-11-13 18:16:50 -0800

    SHow and index page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,10 @@
 class PicsController < ApplicationController
+	before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
 	def index
+	end
 
+	def show
 	end
 
 	def new
@@ -10,13 +13,31 @@ class PicsController < ApplicationController
 
 	def create
 		@pic = Pic.new(pic_params)
+
+		if @pic.save
+			redirect_to @pic, notice: "Yess! It was posted!"
+		else
+			render 'new'
+		end
 	
 	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
 
 	private
 
 	def pic_params
 		params.require(:pic).permit(:title, :description)
+	end
+
+	def find_pic
+		@pic = Pic.find(params[:id])
 	end
 
 
